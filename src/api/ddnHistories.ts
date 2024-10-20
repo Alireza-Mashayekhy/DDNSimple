8538fde720858cf600df9827cdfd5c6484ac3776
Alireza_Mashayekhy <alirezamashayekhy2000@gmail.com>
2024-10-20 13:24:55 +0330

    edit: change autocomplete to dropdown

--- src/api/ddnHistories.ts
+++ src/api/ddnHistories.ts
@@ -1,45 +1,34 @@
-import { getAccess } from "@/utils/authentication";
-import axios from "axios";
+import { getAccess } from '@/utils/authentication';
+import axios from 'axios';
 
 const BASE_URL = `${import.meta.env.VITE_APP_API_URL}/ddn`;
 
 export const getDdnHistories = async (params) => {
-  try {
-    const url = `${BASE_URL}/ddnhistories/`;
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: getAccess(),
-      },
-      params,
-    });
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
-export const exportDdnHistories = async (params) => {
-  try {
-    const url = `${BASE_URL}/ddnhistories/`;
-    const response = await axios.get(url, {
-      params,
-      headers: {
-        Authorization: getAccess(),
-      },
-      responseType: "blob",
-    });
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+    try {
+        const url = `${BASE_URL}/ddnhistories/`;
+        const response = await axios.get(url, {
+            headers: {
+                Authorization: getAccess(),
+            },
+            params,
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 };
 
 export const exportDdnHistories = async (params) => {
     try {
         const url = `${BASE_URL}/ddnhistories/`;
-        const response = await axios.get(url, { params, responseType: 'blob' });
+        const response = await axios.get(url, {
+            params,
+            headers: {
+                Authorization: getAccess(),
+            },
+            responseType: 'blob',
+        });
         return response.data;
     } catch (error) {
         console.error(error);
@@ -48,34 +37,34 @@ export const exportDdnHistories = async (params) => {
 };
 
 export const getDdnDetail = async (id, params) => {
-  try {
-    const url = `${BASE_URL}/customers/${id}/`;
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: getAccess(),
-      },
-      params,
-    });
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+    try {
+        const url = `${BASE_URL}/customers/${id}/`;
+        const response = await axios.get(url, {
+            headers: {
+                Authorization: getAccess(),
+            },
+            params,
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 };
 
 export const exportDdn = async (id, params) => {
-  try {
-    const url = `${BASE_URL}/customers/${id}/export/`;
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: getAccess(),
-      },
-      params,
-      responseType: "blob",
-    });
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+    try {
+        const url = `${BASE_URL}/customers/${id}/export/`;
+        const response = await axios.get(url, {
+            headers: {
+                Authorization: getAccess(),
+            },
+            params,
+            responseType: 'blob',
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 };
